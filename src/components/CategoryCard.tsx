8ca3284dce48965baf99fe594be1d5e7cb498cb4
Livia Xavier <liviaxavier.co@gmail.com>
2024-02-15 09:37:14 -0300

    chore: change category link format

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,18 +1,19 @@
 import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import { CategoryInterface } from "../interfaces/Category";
+import { Link } from "react-router-dom";
 
 export default function CategoryCard({name, image, id}: CategoryInterface){
 
     return   <Grid item xs={12} sm={8} md={4} lg={4}>
     <Card key={id}>
-        <CardActionArea href={`/category/${id}`}>
+        <Link to={`/category/${id}`}>
             <CardMedia sx={{ height: 200 }} image={image} />
             <CardContent>
                 <Typography variant="h5" component="div">
                     {name}
                 </Typography>
             </CardContent>
-        </CardActionArea>
+        </Link>
     </Card>
     </Grid>
 }

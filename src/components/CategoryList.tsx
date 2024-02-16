563623e517d7dec05a56240ac3529e5d87c7b2bb
Livia Xavier <liviaxavier.co@gmail.com>
2024-02-16 15:48:46 -0300

    chore: uses category from google sheets

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,23 +1,45 @@
 import { useCallback, useEffect, useState } from "react"
-import categoryList from '../data/categoryList'
 import CategoryCard from "./CategoryCard"
 import { Grid } from "@mui/material"
 import Search from "./Search"
+import useGoogleSheets from 'use-google-sheets';
 export default function CategoryList() {
-    const [state, setState] = useState({ categoryList })
-    const getCategoryList = useCallback(() => {
-        const list = categoryList
-        setState({ ...state, categoryList: list })
-    }, [])
-    useEffect(() => { getCategoryList() }, [getCategoryList])
-    return <Grid container margin={"auto"} md={8} spacing={2} padding={4}>
+    let initialCategoryList = [{id: '', name: '', image: '', parent: ''}]
+    const [state, setState] = useState({ categoryList: initialCategoryList })
 
+    const { data } = useGoogleSheets({
+        apiKey: import.meta.env.VITE_API_KEY,
+        sheetId: import.meta.env.VITE_SPREADSHEET_ID
+    });
+    
+    const getCategoryList = useCallback( async () => {
+        const categorias = data.find(item => item.id === 'categorias')
+        const gsheetsCategoryList: any = categorias?.data.filter((item: any) => item.name)
+        setState({ ...state, categoryList: gsheetsCategoryList })
+    }, [data])
+
+    useEffect(() => { getCategoryList() }, [getCategoryList])
+
+    return <Grid container margin={"auto"} spacing={2} padding={4}>
         <Search />
-        <Grid container padding={4} spacing={2} sm={12} marginTop={"50px"} >
+       {state.categoryList &&  <Grid container padding={4} spacing={2} marginTop={"50px"} >
 
             {state.categoryList.map(
-                item => <CategoryCard name={item.name} image={item.image} id={item.id} parent={item.parent} />
+                (item:any) => <CategoryCard key={item.id} name={item.name} image={item.image} id={item.id} parent={item.parent} />
             )}
-        </Grid>
+        </Grid>}
     </Grid>
 }
+
+// function GSheets(){
+
+//     if (loading) {
+//         return <div>Loading...</div>;
+//     }
+
+//     if (error) {
+//         return <div>Error!</div>;
+//     }
+
+//     return <div>{JSON.stringify(data)}</div>;
+// }

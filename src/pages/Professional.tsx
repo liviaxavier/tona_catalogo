bf4c4f98b870f1b3a930310feae9011094c98366
Livia Xavier <liviaxavier.co@gmail.com>
2024-02-27 15:40:26 -0300

    chore: add infos to profile

--- src/pages/Professional.tsx
+++ src/pages/Professional.tsx
@@ -1,7 +1,8 @@
 import { Link, useParams } from "react-router-dom";
-import { Breadcrumbs, Grid, Typography } from "@mui/material";
+import { Box, Breadcrumbs, Chip, Grid, Typography } from "@mui/material";
 import '../styles/professional.css'
 import Search from "../components/Search";
+import { MdCamera, MdFace } from "react-icons/md";
 
 export default function ProfessionalPage({data}: DataBindInterface){
     const {professionalId} = useParams()
@@ -9,10 +10,12 @@ export default function ProfessionalPage({data}: DataBindInterface){
     const category = data[Number(categoryId)]
     const professionalCode = professionalId?.split("p")[1]
     const professional = category?.data[Number(professionalCode) - 1] || {}
+    const {Presencial, Online} = professional
+    const tel = professional["Contato divulgação"].replace(/^(\+)|\D/g, "$1")
     if(!professional) return "loading..."
     return professional && <>
     <Search />
-    <Grid sm={12} margin={"auto"} container spacing={2} padding={4}>
+    <Grid sm={12} margin={"auto"} container spacing={2}>
         <Grid item sm={12} >
             <Breadcrumbs aria-label="breadcrumb">
                 <Link  color="inherit" to="/">
@@ -34,8 +37,13 @@ export default function ProfessionalPage({data}: DataBindInterface){
         </Grid>
         <Grid item sm={12} md={8}>
             <h1>{professional.Nome}</h1>
+            <p>{professional["Localização"]}</p>
+            <Box component={"div"} className="professional__chips">
+                {Presencial === 's' && <Chip icon={<MdFace/>} label={"presencial"} />}
+                {Online === 's' && <Chip icon={<MdCamera />} label={"online"} />}
+            </Box>
             <br></br>
-            <a target="_blank" href={`https://wa.me/55${professional["Contato divulgação"]}`}>Telefone: {professional["Contato divulgação"]}</a>
+            <a target="_blank" href={`https://wa.me/55${tel}`}>Telefone: {professional["Contato divulgação"]}</a>
         </Grid>
         <Grid item sm={12}>
             <h3>Descrição</h3>

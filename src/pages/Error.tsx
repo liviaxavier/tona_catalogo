2ef85ff55a0411d61f623ca81fbab51a57ae5b1a
Livia Xavier <liviaxavier.co@gmail.com>
2024-07-25 11:43:14 -0300

    feat: adiciona botao de redirecionamento para pagina inical em pagina de erro

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,16 +1,30 @@
-import { useRouteError } from "react-router-dom";
+import { Button, Grid } from "@mui/material";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Grid container height={'70vh'} alignItems={'center'} justifyContent={'center'} >
+      <Grid item xs={12} height={'10vh'}>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+
+      </Grid>
+      <Grid xs={12}>
+        <Link to="/" className='card-button'>
+          <Button
+            style={{ width:'100%' }}
+            size="large" color="primary" variant='text'>
+            Ir para página inicial
+          </Button>
+        </Link>
+
+      </Grid>
+    </Grid>
   );
 }
